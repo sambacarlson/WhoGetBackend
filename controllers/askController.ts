f0815ef5ab9ed9dd5d4e830269a95e0b2558e0d1
Kwahnwe <60662513+sambacarlson@users.noreply.github.com>
2023-04-24 16:20:25 +0100

    Dev (#1)
    * Implimented get asks by categories, enabled strict type checking and fixed req/res types in controllers
    
    * Added filters for querying Banned and Hidden users and asks respectively.

--- controllers/askController.ts
+++ controllers/askController.ts
@@ -1,4 +1,5 @@
 import Ask from "../models/askModel";
+import Express from "express";
 
 /** METHODS:
  * ask_all_get
@@ -8,43 +9,67 @@ import Ask from "../models/askModel";
  * ask_hide_patch
  */
 
-/** Get all asks from database */
-const ask_all_get = (req, res) => {
-  Ask.find()
-    .then(result => res.status(200).json(result))
-    .catch(err => res.status(400).json({message: err.message}));
+/** Get all asks from database. takes a 'queryOptions' object as req.body specifying caterogries to choose from. */
+const ask_all_get = (req: Express.Request, res: Express.Response) => {
+  const queryOptions: { categories?: [string], showHidden?: boolean, onlyHidden?: boolean } = req.body;
+  /** hide Hidden defaults to true */
+  /// Only hidden
+  if (queryOptions.onlyHidden) {
+    Ask.find({ "status.hidden": { $eq: true } })
+      .then(result => res.status(200).json(result))
+      .catch(err => res.status(400).json({ message: err.message }));
+  }
+  /// All asks regardless
+  else if (queryOptions.showHidden === true) {
+    Ask.find({})
+      .then(result => res.status(200).json(result))
+      .catch(err => res.status(400).json({ message: err.message }));
+  }
+  /// Categorized unhidden
+  else if (queryOptions.categories !== undefined) {
+    Ask.find({ $and: [{ categories: { $in: queryOptions.categories } }, { "status.hidden": { $eq: false } }] })
+      .then(result => res.status(200).json(result))
+      .catch(err => res.status(400).json({ message: err.message }));
+  }
+  /// All unhidden
+  else {
+    Ask.find({ "status.hidden": { $eq: false } })
+      .then(result => res.status(200).json(result))
+      .catch(err => res.status(400).json({ message: err.message }));
+  }
+
 }
 
 /** Add and execise to the database. ensure all required attributes are available with their right types */
-const ask_create_post = (req, res) => {
+const ask_create_post = (req: Express.Request, res: Express.Response) => {
   const ask = new Ask(req.body);
   ask.save()
     .then(result => res.status(201).json(result))
-    .catch(err => res.status(400).json({message: err.message}));
+    .catch(err => res.status(400).json({ message: err.message }));
 }
 
 /** Edit an existing ask on the database with entry */
-const ask_edit_patch = (req, res) => {
+const ask_edit_patch = (req: Express.Request, res: Express.Response) => {
   const id = req.params.id;
   Ask.findByIdAndUpdate(id, req.body)
-  .then(() => res.status(201).end())
-  .catch(err => res.status(400).json({message: err.message}));
+    .then(() => res.status(201).end())
+    .catch(err => res.status(400).json({ message: err.message }));
 }
 
 /** Deletes the ask with id provided to the req params */
-const ask_delete = (req, res) => {
+const ask_delete = (req: Express.Request, res: Express.Response) => {
   const id = req.params.id;
   Ask.findByIdAndDelete(id)
     .then(() => res.status(200).end())
-    .catch(err => res.status(400).json({message: err.message}));
+    .catch(err => res.status(400).json({ message: err.message }));
 }
 /** Hide the ask with given id */
-const ask_hide_patch = (req, res) => {
+const ask_hide_patch = (req: Express.Request, res: Express.Response) => {
   const id = req.params.id;
   Ask.findByIdAndUpdate(id, req.body)
     .then(() => res.status(200).end())
-    .catch(err => res.status(400).json({message: err.message}));
+    .catch(err => res.status(400).json({ message: err.message }));
 }
 
 // Export all methods
-export {ask_all_get, ask_create_post, ask_edit_patch, ask_delete, ask_hide_patch};
+export { ask_all_get, ask_create_post, ask_edit_patch, ask_delete, ask_hide_patch };
