a718c092fe2826115f806a9693b8abc0504aa62b
Carlson Samba <sambacarlson@gmail.com>
2023-05-16 06:19:26 +0100

    Updated User constroller to create user if not exist and update user if already exists

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import {
   user_get,
-  user_create_post,
+  user_create_put,
   user_edit_patch,
 } from "../controllers/userController";
 
 const router = express.Router();
 router.get("/", user_get);
-router.post("/", user_create_post);
+router.put("/", user_create_put);
 
 //routes with cutom parts should always be placed at the bottom
 router.patch("/:id", user_edit_patch);
